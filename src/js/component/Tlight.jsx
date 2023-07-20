bb839c7b11fae5e18555951634eb6cf6cdaea83a
BiancaReset <130721122+BiancaReset@users.noreply.github.com>
2023-07-20 00:30:04 +0000

    19julio

---
+++ src/js/component/Tlight.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Tlight = () => {
+  const [activeLight, setActiveLight] = useState(null);
+
+  const handleLightClick = (color) => {
+    setActiveLight(color);
+  };
+
+  return (
+    <div id="traffic-light" className="container">
+      <div
+        className={`light red ${activeLight === 'red' ? 'active' : ''}`}
+        onClick={() => handleLightClick('red')}
+      ></div>
+      <div
+        className={`light yellow ${activeLight === 'yellow' ? 'active' : ''}`}
+        onClick={() => handleLightClick('yellow')}
+      ></div>
+      <div
+        className={`light green ${activeLight === 'green' ? 'active' : ''}`}
+        onClick={() => handleLightClick('green')}
+      ></div>
+    </div>
+  );
+};
+
+export default Tlight;
